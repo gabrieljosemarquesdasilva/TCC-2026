5c76a34e748e8d58184e4438cfe814a171875fd3
ryu <139727681+Ryutq@users.noreply.github.com>
2026-04-16 08:59:21 -0300

    .
    .

---
+++ site_beach_tennis/site_beach/banco.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE agendamento;
+
+USE agendamento;
+
+CREATE TABLE agendamentos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100),
+    quadra VARCHAR(50),
+    data DATE,
+    horario TIME,
+    modalidade VARCHAR(50),
+    nivel VARCHAR(50)
+);
+
+node server.js 
+
