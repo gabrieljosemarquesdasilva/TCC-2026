2fef7190c3cecebc1b38c8746912c90178348a3d
Arthur Lima <aarthurxx@gmail.com>
2026-04-22 07:56:30 -0300

    22/04

--- site_beach_novo/banco.sql
+++ site_beach_novo/banco.sql
@@ -12,5 +12,17 @@ CREATE TABLE agendamentos (
     nivel VARCHAR(50)
 );
 
+CREATE DATABASE usuarios;
+
+USE usuarios;
+
+CREATE TABLE login (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  google_id VARCHAR(255),
+  email VARCHAR(255),
+  nome VARCHAR(255),
+  picture TEXT
+);
+
 node server.js 
 
